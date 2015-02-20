66caf8442aa4abcb3da0d09b0ec852db64313fd1
Gus Bonfant <gbonfant@me.com>
2015-02-20 15:16:55 +0000

    Implemented frontend for rendering components

---
+++ app/js/application.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var RepositoryList = require('./repository_list.jsx');
+var component = <RepositoryList user="gbonfant" />;
+
+React.render(component, document.getElementById('react-component'));
