46caf736bfefeacbdf87601c1de66048c78bfe47
Gus Bonfant <gbonfant@me.com>
2015-02-23 11:06:41 +0000

    Updated RepositoryList component to sort repos

--- app/js/repository_list.jsx
+++ app/js/repository_list.jsx
@@ -1,25 +1,39 @@
 var $ = require('jquery');
 var React = require('react');
 var Repository = require('./repository.jsx');
-var repos = require('./repos.js');
 var RepositoryList = React.createClass({
   getInitialState: function() {
     return { initialRepoList: [] };
   },
 
   componentDidMount: function() {
-    this.setState({ initialRepoList: repos });
-    // this.fetchRepos(function(repositories) {
-    //   if (this.isMounted()) {
-    //     this.setState({ initialRepoList: repositories });
-    //   }
-    // }.bind(this));
+    this.fetchRepos(function(repositories) {
+      if (this.isMounted()) {
+        var sortedRepos = this.sortByStars(repositories);
+
+        this.setState({ initialRepoList: sortedRepos });
+      }
+    }.bind(this));
   },
 
   fetchRepos: function(callback) {
     var url = "https://api.github.com/users/" + this.props.user + "/repos";
 
     return $.get(url, callback);
+  },
+
+  sortByStars: function(repositories) {
+    return repositories.sort(function(a, b) {
+      if (a.stargazers_count > b.stargazers_count) {
+        return -1;
+      }
+
+      if (a.stargazers_count < b.stargazers_count) {
+        return 1;
+      }
+
+      return 0;
+    });
   },
 
   render: function() {
